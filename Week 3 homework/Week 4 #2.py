03f577aeb124673b9efc47aa64694c8aa13940eb
NevillAdeyeye <hiimnevill@gmail.com>
2024-07-09 21:46:45 -0700

    Create Week 4 #2.py

---
+++ Week 3 homework/Week 4 #2.py
@@ -0,0 +1,11 @@
+def main ():
+    scores = [0, 0, 0, 0, 0]
+    for i in range(5):
+        getJudgesScores()
+
+def getJudgesScores ():
+    scores = int(input("Enter score for judge {}: ".format(i + 1)))
+def findLowestScore(score1, score2, score3, score4, score5):
+    lowest = score1
+def findHighestScore(score1, score2, score3, score4, score5):
+    highest = score1
